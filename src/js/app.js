3c90cc760271b2220ad1ff1bda434c46ff7d466a
Amal Muslim <muslim.amal@yandex.ru>
2019-05-06 17:37:08 +0300

    add delete game

--- src/js/app.js
+++ src/js/app.js
@@ -7,8 +7,15 @@ import {Game} from './game.js';
 glide();
 
 let game = new Game ();
-game.getListOfGames().then(games => {
-  console.log(games);
+
+// game.getListOfGames().then(games => {
+//   console.log(games);
+// });
+
+// game.getGame(1).then(game => {
+//   console.log(game);
+// });
+
+game.deleteGame(5).then(g => {
+  console.log(g);
 });
-
-
--- src/js/game.js
+++ src/js/game.js
@@ -1,20 +1,50 @@
 import axios from 'axios';
+import qs from 'qs';
 
 export class Game {
 
-  getGame() {}
+  getGame(id) {
+    let game = axios({
+      method:'get',
+      url:'http://localhost/get.php',
+      params : {
+        id:id
+      },
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(function(response) {
+        return response.data;
+      });
+    return game;
+  }
+
   getListOfGames() {
     let games = axios({
       method:'get',
       url:'/api/database.json',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      headers: {'Content-Type': 'application/json'}
     })
       .then(function(response) {
         return response.data;
       });
     return games;
   }
-  deleteGame() {}
+
+  deleteGame(id) {
+
+    let bodyFormData = new FormData();
+    bodyFormData.set('id','5');
+
+    let game = axios({
+      method: 'post',
+      url: 'http://localhost/delete.php',
+      data: bodyFormData,
+      config : {headers: {'Content-Type': 'application/json' }},
+    }).then (function(response) {
+      return response;
+    });
+    return game;
+  }
   addGame() {}
   editGame() { }
   vote() {}
