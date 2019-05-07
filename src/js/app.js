048c03ee0c14acf62cb57bdb96c658bae4c33bfd
Amal Muslim <muslim.amal@yandex.ru>
2019-05-07 14:19:26 +0300

    fix merge

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,13 @@ import {glide} from './lib/glide.js';
 import {Game} from './game.js';
 glide();
 
-<<<<<<< HEAD
-let game = [{},{},{}];
-
-
-=======
 let game = new Game ();
 
 // CRUD
->>>>>>> server
 // game.getListOfGames().then(games => {
 //   console.log(games);
 // });
 
-<<<<<<< HEAD
-// let gameItem = document.querySelector('.content__game-all-list');
-// gameItem.innerHTML += (dustyEngine(allGameTemplate()));
-=======
 // game.getGame(2).then(game => {
 //   console.log(game || 'game not found' );
 // });
@@ -41,7 +31,6 @@ let game = new Game ();
 // game.voteGame(12,4).then(game => {
 //   console.log(game || 'game not found' );
 // });
->>>>>>> server
 
 
 document.querySelector('.content__game-all-list').appendChild(
