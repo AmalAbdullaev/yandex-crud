abfccb5a97cf2e23bd752a0b76072e88bc9ee13d
Amal Muslim <muslim.amal@yandex.ru>
2019-05-13 17:19:31 +0300

    filtering

--- src/js/controller/listeners.controller.js
+++ src/js/controller/listeners.controller.js
@@ -13,6 +13,10 @@ module.exports = function listeneres() {
   let steamPlatformFilter = document.querySelector('.modal__platform--steam');
   let psPlatformFilter = document.querySelector('.modal__platform--ps');
   let xboxPlatformFilter = document.querySelector('.modal__platform--xbox');
+  let descFilter = document.querySelector('.content__filter-ico--up');
+  let ascFilter = document.querySelector('.content__filter-ico--down');
+  let refresh = document.querySelector('.content__filter-ico--refresh');
+
 
   let modal = new VanillaModal();
 
@@ -28,6 +32,7 @@ module.exports = function listeneres() {
   let formPlatformWindows = document.querySelector('.modal__platform--windows');
   let formPlatformMac = document.querySelector('.modal__platform--mac');
   let formPlatformPlayStation = document.querySelector('.modal__platform--ps');
+  
 
 
   form.onsubmit = function(event) {
@@ -94,42 +99,87 @@ module.exports = function listeneres() {
   };
 
   macPlatformFilter.onclick = function() {
-    console.log('mac');
-    platformFilter();
+    gameModel.getListOfGames().then(games => {
+      renderGames('.content__game-all-list', desc(asc(platformFilter(games))));
+    });
   };
 
   windowsPlatformFilter.onclick = function() {
-    console.log('windows', windowsPlatformFilter.checked);
-    platformFilter();
+    gameModel.getListOfGames().then(games => {
+      renderGames('.content__game-all-list', desc(asc(platformFilter(games))));
+    });
   };
   steamPlatformFilter.onclick = function() {
-    platformFilter();
+    gameModel.getListOfGames().then(games => {
+      renderGames('.content__game-all-list', desc(asc(platformFilter(games))));
+    });
   };
   psPlatformFilter.onclick = function() {
-    console.log('ps');
-    platformFilter();
+    gameModel.getListOfGames().then(games => {
+      renderGames('.content__game-all-list', desc(asc(platformFilter(games))));
+    });
   };
   xboxPlatformFilter.onclick = function() {
-    console.log('xbox');
-    platformFilter();
+    gameModel.getListOfGames().then(games => {
+      renderGames('.content__game-all-list', desc(asc(platformFilter(games))));
+    });
   };
 
-  function platformFilter() {
-    gameModel.getListOfGames().then(games => {
-      let result = [];
-      if(xboxPlatformFilter.checked)
-        games.filter(e => e.platform.includes('Xbox')).forEach(e => result.push(e));
-      if(windowsPlatformFilter.checked)
-        games.filter(e => e.platform.includes('Windows')).forEach(e => result.push(e));
-      if(macPlatformFilter.checked)
-        games.filter(e => e.platform.includes('MacOs')).forEach(e => result.push(e));
-      if(psPlatformFilter.checked)
-        games.filter(e => e.platform.includes('PlayStation')).forEach(e => result.push(e));
-      if(steamPlatformFilter.checked)
-        games.filter(e => e.platform.includes('SteamOS')).forEach(e => result.push(e));
+  function platformFilter(games) {
+    let result = [];
+    if(xboxPlatformFilter.checked)
+      games.filter(e => e.platform.includes('Xbox')).forEach(e => result.push(e));
+    if(windowsPlatformFilter.checked)
+      games.filter(e => e.platform.includes('Windows')).forEach(e => result.push(e));
+    if(macPlatformFilter.checked)
+      games.filter(e => e.platform.includes('MacOs')).forEach(e => result.push(e));
+    if(psPlatformFilter.checked)
+      games.filter(e => e.platform.includes('PlayStation')).forEach(e => result.push(e));
+    if(steamPlatformFilter.checked)
+      games.filter(e => e.platform.includes('SteamOS')).forEach(e => result.push(e));
 
-      if(result.length > 0) renderGames('.content__game-all-list', result);
-      else renderGames('.content__game-all-list', games);
-    });
+    if(result.length === 0)
+      result = games;
+    return result;
   }
+
+  ascFilter.onclick = function() {
+    gameModel.getListOfGames().then(games => {
+      descFilter.checked = false;
+      renderGames('.content__game-all-list', asc(platformFilter(games)));
+    });
+  };
+
+  descFilter.onclick = function() {
+    gameModel.getListOfGames().then(games => {
+      ascFilter.checked = false;
+      renderGames('.content__game-all-list', desc(platformFilter(games)));
+    });
+  };
+
+  function desc(games) {
+    if(descFilter.checked) 
+      return games.sort((a,b) => b.rating - a.rating);
+    else return games;
+  }
+
+  function asc(games) {
+    if(ascFilter.checked)
+      return games.sort((a,b) => a.rating - b.rating );
+    else return games;
+  }
+ 
+  refresh.onclick = function() {
+    gameModel.getListOfGames().then(games => {
+      refresh.checked = false;
+      macPlatformFilter.checked = false;
+      windowsPlatformFilter.checked = false;
+      steamPlatformFilter.checked = false;
+      psPlatformFilter.checked = false;
+      xboxPlatformFilter.checked = false;
+      descFilter.checked = false;
+      ascFilter.checked = false;
+      renderGames('.content__game-all-list', games);
+    });
+  };
 };
